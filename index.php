dff0e6fce9dc545ef3d85502f7e8620cbf71b348
Steffen Martinsen <steffen@steffenmedia.no>
2012-02-03 17:32:06 +0100

    GUI nedstrapping - kun jobbing mot data

--- index.php
+++ index.php
@@ -5,77 +5,6 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
 <meta charset="UTF-8">
-<style type="text/css">
-body {
-background: #adbc0a;
-margin:0;
-padding:0;
-}
-
-* {
-margin:0;
-padding:0;
-}
-
-#kontainer {
-width:900px;
-margin:0 auto;
-}
-.ui-autocomplete { height: 200px; overflow-y: scroll; overflow-x: hidden;}
-
-.fratil {
-position: absolute;
-background: #fff;
-padding:10px;
-width:340px;
-margin-left:300px;
-box-shadow: 2px 2px 1px #7d8807;
-}
-
-.fratil input {
-width:300px;
-outline: none;
-border:none;
-}
-
-.fratil #clickme {
-position: absolute;
-margin-top:-5px;
-margin-left:10px;
-}
-
-#kortAbout {
-margin-top:300px;
-position: absolute;
-font-family:Helvetica;
-font-size:12px;
-margin-left:240px;
-}
-
-#kortAbout ul {
-list-style: none;
-}
-
-#kortAbout li {
-display: inline;
-margin-right:50px;
-}
-
-#kortAbout a {
-color:purple;
-text-decoration: none;
-}
-
-#kortAbout a:hover {
-text-decoration: underline;
-}
-
-#toppen {
-margin-left:240px;
-margin-top:40px;
-}
-</style>
-
 </head>
 <body>
 
@@ -85,10 +14,10 @@ margin-top:40px;
 		<?php
 		include('mysql-config.php');
 		
-$result = mysql_query("SELECT DISTINCT stop_name AS holdeplass FROM stops");
-while($row = mysql_fetch_array($result)) {
-echo '"' . $row['holdeplass'] .'",'; 
-}
+	$result = mysql_query("SELECT DISTINCT stop_name AS holdeplass FROM stops");
+	while($row = mysql_fetch_array($result)) {
+	echo '"' . $row['holdeplass'] .'",'; 
+	}
 
 		?>
 		];
@@ -105,42 +34,24 @@ echo '"' . $row['holdeplass'] .'",';
 
 	
 <div id="kontainer">
-<img id="toppen" src="topp.png"/>
 <div class="ui-widget">
 
 	<div id="fraPlass" class="fratil">
-	<input id="from" placeholder="Kor henne e du?">
-	<a id="clickme" href="#"><img src="off.png"></a>
-	</div>
 	
+	<label for="from">Fra:</label>
+	<input id="from" placeholder="Fra holdeplass">
 	
-	<div style="display:none"id="tilPlass" class="fratil">
-	<input id="to" placeholder=".. å kor vil du hen?">
-	<a id="clickme" href="#"><img src="off.png"></a>
+<br/><br/>
+	
+	<label for="to">Til:</label>
+	<input id="to" placeholder="Fra holdeplass">
+	
+<br/><br/>
 	</div>
 
 </div>
 
-
-
-<script>
-$('#clickme').click(function() {
-        $('#fraPlass').fadeOut();
-        $('#tilPlass').show("slide", { direction: "right" }, 300);
-
-    });
-</script>
-
-<div id="kortAbout">
-<ul>
-<li><a href="#">XML API</a></li>
-<li><a href="#">JSON API</a></li>
-<li><a href="#">Retningslinjer</a></li>
-<li><a href="http://www.twitter.com/kverdagshelt">Twitter</a></li>
-<li><a href="#">Om meg</a></li>
-</ul>
 </div>
 
-</div>
 </body>
 </html>
